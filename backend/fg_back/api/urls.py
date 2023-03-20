58641b72110c9a4b5e0f6fe893b8eecbf67b0347
am <am@pop-os.localdomain>
2023-03-20 18:11:13 +0100

    YAT Mon Mar 20 06:11:13 PM CET 2023

---
+++ backend/fg_back/api/urls.py
@@ -0,0 +1,13 @@
+from django.urls import include, path
+# from rest_framework.routers import DefaultRouter
+
+
+# router_api_v1 = DefaultRouter()
+
+# router_api_v1.register("users", views.UserViewSet)
+
+
+urlpatterns = [
+    path("", include("djoser.urls")),
+    path("auth/", include("djoser.urls.authtoken")),
+]
